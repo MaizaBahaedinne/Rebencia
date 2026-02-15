44dc1f5ad5d5957126fd15afeaf5b7ca0f70ea9b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2026-02-15 23:55:09 +0100

    feat: Ajouter la colonne notary à la table transactions

---
+++ app/Database/Migrations/2026-02-15-232000_AddNotaryToTransactions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddNotaryToTransactions extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'notary' => [
+                'type' => 'VARCHAR',
+                'constraint' => 150,
+                'null' => true,
+                'after' => 'contract_number',
+            ],
+        ];
+
+        $this->forge->addColumn('transactions', $fields);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('transactions', 'notary');
+    }
+}
